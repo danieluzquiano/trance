f56d4d2d3686b1d6ad21b02489cbd0ab1402bf11
danieluzquiano <55982609+danieluzquiano@users.noreply.github.com>
2022-12-06 14:14:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let mX, mY
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
   mX = windowWidth / 2;
   mY = windowHeight / 2;
 }
@@ -10,17 +11,17 @@ function setup() {
 function draw() {
   background(0);
   translate(width / 2, height / 2);
-  scale(5);
-  let mm = map(mX, 0, width, -1, 1);
-  let my = map(mY, 0, height, -1, 1);
-
+  scale(80);
+  let mm = map(mX, 0, width, -3, 3);
+  let my = map(mY, 0, height, 3, -3);
+  noFill()
   for (let a = 0; a < 2.5; a += 0.0004) {
     stroke(255)
-    strokeWeight(0.151)
+    strokeWeight(0.0128)
     let x = a*10000;
-    let y = (a)/tan(x+zoff)+mm;
-    let e = (cos(x))*80;
-    circle(sin(tan(cos(y))+mm)*e, sin(tan(sin(y))+mm)*e,0.03);
+    let y = (a)/tan(x+zoff)+my;
+    let e = (cos(x))*1.3;
+    circle(cos(tan(sin(y+my)))/e, (((y-mm)))*a, 0.0031);
   }
   zoff += 0.035;
 }
